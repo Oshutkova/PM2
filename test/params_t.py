f79152181f232cfaed8f053784008d70f733294c
Rudenko_Darya <46627206+Oshutkova@users.noreply.github.com>
2024-03-23 11:07:54 +0300

    Update params_t.py

--- test/params_t.py
+++ test/params_t.py
@@ -1,1 +1,8 @@
+import sys
 
+from CalculatePeople import find_people as fp
+from CalculatePeople import result_write_base as rws
+
+fp.kamera = 'https://github.com/Oshutkova/PM2/blob/6479d0b1f593e8964e4909bb3c19d746a19b37cc/test/ochered%20(1).jpg'
+spisok_kamer = 'kamera.file_name_obr'
+rws.spisok_kamer.conn ='spisok_kamer', 3
