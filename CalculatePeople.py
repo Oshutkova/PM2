24eb38a71282c24aa0a04f77e0a2d8f78d71fca2
aapopov52 <149938611+aapopov52@users.noreply.github.com>
2024-03-23 11:34:26 +0300

    выполнены требования flake8
    выполнены требования flake8

--- CalculatePeople.py
+++ CalculatePeople.py
@@ -29,30 +29,32 @@ class Kamera_class:
     url = ''
     usl_send_less = 0   # сообщение уйдёт если значение будет меньше заданного
     usl_send_more = 0   # сообщение уйдёт если значение будет больше заданного
-    usl_change_min = 0  # при изменении количества на заданную величину 
-                        # (и усугубления ситуации) - повторное сообщение
+    usl_change_min = 0  # ухудшение ситуации на величину
     usl_norm_less = 0   # нормализация условий после снижения
     usl_norm_more = 0   # нормализация условий псле превышения
     cnt_people = 0      # сколько человек определено на картинке
     folder_name = ''    # папка куда загружен файл
     file_name = ''      # имя загруженного файла с картинкой
     file_name_obr = ''  # имя обработанного фала (расчерчены люди)
-    #УСловие превышения
-    last_send_more_usl_dt = datetime.now()   # дата/время посл сообщ по условиям
+    # условия на превышение
+    last_send_more_usl_dt = datetime.now()   # дата/вр посл сообщ по условиям
     last_send_more_usl_cnt = 0   # количество людей в последнем сообщении
-    last_send_more_norm_dt = datetime.now()  # дата/время посл сообщ по нормализации
-    last_send_more_norm_cnt = 0  # кол-во людей в последнем сообщении по нормал
-    # Условние снижения
-    last_send_less_usl_dt = datetime.now()   # дата/время посл сообщ по условиям
+    last_send_more_norm_dt = datetime.now()  # дата/вр посл сообщ по нормал
+    last_send_more_norm_cnt = 0  # кол-во лиц в последнем сообщении по нормал
+    # условия на снижение
+    last_send_less_usl_dt = datetime.now()   # дата/вр посл сообщ по условиям
     last_send_less_usl_cnt = 0   # количество людей в последнем сообщении
-    last_send_less_norm_dt = datetime.now()  # дата/время посл сообщ по нормализации
-    last_send_less_norm_cnt = 0  # кол-во людей в последнем сообщении по нормал
- 
-    b_add_mess_usl_more = 0      # признак - создать сообщение на выполнение для условия превышения
-    b_add_mess_usl_more_norm = 0     # признак - созддать сообщение о нормализации для условия превышения
-    b_add_mess_usl_less = 0      # признак - создать сообщение на выполнение для условия на снижение
-    b_add_mess_usl_less_norm = 0     # признак - созддать сообщение о нормализации для условия на снижение
+    last_send_less_norm_dt = datetime.now()  # дата/вр посл сообщ по нормал
+    last_send_less_norm_cnt = 0  # кол-во лиц в последнем сообщении по нормал
+    # Создать сообщения для услолвия превышения:
+    b_add_mess_usl_more = 0       # создать сообщение для условия превышения
+    b_add_mess_usl_more_norm = 0  # создать сообщение о нормализации
+    # Создать сообщения для услолвия снижения:
+    b_add_mess_usl_less = 0       # создать сообщение для условия превышения
+    b_add_mess_usl_less_norm = 0  # создать сообщение о нормализации
 
+
+# функция по выделению людей на картинке
 def find_people(kamera):
     # Загрузка модели и весов YOLOv3
     net = cv2.dnn.readNet("yolov3.weights", "darknet/cfg/yolov3.cfg")
@@ -79,6 +81,8 @@ def find_people(kamera):
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             # Проверка на наличие класса человека
+            # 0 - класс человека
+            # 0,5 - вероятность выше 0,5
             if confidence > 0.5 and class_id == 0:
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
@@ -166,7 +170,8 @@ def get_konstant(konstant_name, konstants, type_out):
             elif type_out == 3:
                 try:
                     return datetime.strptime(konstant.value, '%d.%m.%Y')
-                except:
+                except Exception as e:
+                    print(f"Ошибка преобразования в дату: {e}")
                     return ''
     return ''
 
@@ -175,35 +180,50 @@ def get_konstant(konstant_name, konstants, type_out):
 def get_spisok_kamer(conn):
     kameras = []
     cur = conn.cursor()
-    sql = """ 
-        select C.id, C.name, C.url, C.usl_send_less, C.usl_send_more, C.usl_change_min,
-               C.usl_norm_less, C.usl_norm_more,
-               t_usl_more_last.date_time last_send_more_usl_dt,  t_usl_more_last.cnt_people last_send_more_usl_cnt,
-               t_more_norm_last.date_time last_send_more_norm_dt,  t_more_norm_last.cnt_people last_send_more_norm_cnt,
-               t_usl_less_last.date_time last_send_less_usl_dt,  t_usl_less_last.cnt_people last_send_less_usl_cnt,
-               t_less_norm_last.date_time last_send_less_norm_dt,  t_less_norm_last.cnt_people last_send_less_norm_cnt
+    sql = """
+        select C.id, C.name, C.url, C.usl_send_less, C.usl_send_more,
+               C.usl_change_min, C.usl_norm_less, C.usl_norm_more,
+               t_usl_more_last.date_time last_send_more_usl_dt,
+               t_usl_more_last.cnt_people last_send_more_usl_cnt,
+               t_more_norm_last.date_time last_send_more_norm_dt,
+               t_more_norm_last.cnt_people last_send_more_norm_cnt,
+               t_usl_less_last.date_time last_send_less_usl_dt,
+               t_usl_less_last.cnt_people last_send_less_usl_cnt,
+               t_less_norm_last.date_time last_send_less_norm_dt,
+               t_less_norm_last.cnt_people last_send_less_norm_cnt
         from calc_people_camera C
+
         left join (select tc.id_calc_people_camera, max(id) id_max
-                           from calc_people_camera_cnt_people tc
-                           where tc.b_add_mess_usl_more = True
-                           group by tc.id_calc_people_camera) t_usl_more_last_id on t_usl_more_last_id.id_calc_people_camera = C.id
-          left join calc_people_camera_cnt_people t_usl_more_last on t_usl_more_last.id = t_usl_more_last_id.id_max
+                     from calc_people_camera_cnt_people tc
+                     where tc.b_add_mess_usl_more = True
+                     group by tc.id_calc_people_camera) t_usl_more_last_id on
+                                t_usl_more_last_id.id_calc_people_camera = C.id
+          left join calc_people_camera_cnt_people t_usl_more_last on
+                                t_usl_more_last.id = t_usl_more_last_id.id_max
+
         left join (select tc.id_calc_people_camera, max(id) id_max
-                           from calc_people_camera_cnt_people tc
-                           where tc.b_add_mess_usl_more_norm = True
-                           group by tc.id_calc_people_camera) t_usl_more_norm_id on t_usl_more_norm_id.id_calc_people_camera = C.id
-        left join calc_people_camera_cnt_people t_more_norm_last on t_more_norm_last.id = t_usl_more_norm_id.id_max   
-                  
+                     from calc_people_camera_cnt_people tc
+                     where tc.b_add_mess_usl_more_norm = True
+                     group by tc.id_calc_people_camera) t_usl_more_norm_id on
+                                t_usl_more_norm_id.id_calc_people_camera = C.id
+        left join calc_people_camera_cnt_people t_more_norm_last on
+                                t_more_norm_last.id = t_usl_more_norm_id.id_max
+
               left join (select tc.id_calc_people_camera, max(id) id_max
-                           from calc_people_camera_cnt_people tc
-                           where tc.b_add_mess_usl_less = True
-                           group by tc.id_calc_people_camera) t_usl_less_last_id on t_usl_less_last_id.id_calc_people_camera = C.id
-          left join calc_people_camera_cnt_people t_usl_less_last on t_usl_less_last.id = t_usl_less_last_id.id_max
+                     from calc_people_camera_cnt_people tc
+                     where tc.b_add_mess_usl_less = True
+                     group by tc.id_calc_people_camera) t_usl_less_last_id on
+                                t_usl_less_last_id.id_calc_people_camera = C.id
+          left join calc_people_camera_cnt_people t_usl_less_last on
+                                t_usl_less_last.id = t_usl_less_last_id.id_max
+
         left join (select tc.id_calc_people_camera, max(id) id_max
-                           from calc_people_camera_cnt_people tc
-                           where tc.b_add_mess_usl_less_norm = True
-                           group by tc.id_calc_people_camera) t_usl_less_norm_id on t_usl_less_norm_id.id_calc_people_camera = C.id
-          left join calc_people_camera_cnt_people t_less_norm_last on t_less_norm_last.id = t_usl_less_norm_id.id_max    
+                     from calc_people_camera_cnt_people tc
+                     where tc.b_add_mess_usl_less_norm = True
+                     group by tc.id_calc_people_camera) t_usl_less_norm_id on
+                                t_usl_less_norm_id.id_calc_people_camera = C.id
+          left join calc_people_camera_cnt_people t_less_norm_last on
+                                t_less_norm_last.id = t_usl_less_norm_id.id_max
         """
 
     cur.execute(sql)
@@ -275,7 +295,7 @@ def get_spisok_kamer(conn):
             kamera.last_send_less_norm_cnt = row[15]
         else:
             kamera.last_send_less_norm_cnt = -1
-         
+
         kameras.append(kamera)
     cur.close()
 
@@ -324,33 +344,45 @@ def clear_photo_folder(spisok_kamer, cnt_file):
 def usl_send_mess(spisok_kamer, konstants):
     min_inrerval_sec = get_konstant('Мин интервал оповещения - сек',
                                     konstants, 2)
-    
+
     for i, kam in enumerate(spisok_kamer):
         # сообщение на выполнения условия превышения
         if kam.usl_send_more > 0 and kam.cnt_people > kam.usl_send_more and \
-           (kam.last_send_more_usl_dt == datetime(1900, 1, 1) or \
-            (datetime.now() - kam.last_send_more_usl_dt).total_seconds() > min_inrerval_sec or \
-            (kam.last_send_more_norm_dt != datetime(1900, 1, 1) and kam.last_send_more_norm_dt > kam.last_send_more_usl_dt) or \
-            kam.cnt_people > kam.last_send_more_usl_cnt + kam.usl_change_min):
+           (
+             kam.last_send_more_usl_dt == datetime(1900, 1, 1) or
+             (datetime.now() - kam.last_send_more_usl_dt).total_seconds() >
+             min_inrerval_sec or
+             (kam.last_send_more_norm_dt != datetime(1900, 1, 1) and
+              kam.last_send_more_norm_dt > kam.last_send_more_usl_dt) or
+             kam.cnt_people > kam.last_send_more_usl_cnt + kam.usl_change_min
+           ):
             spisok_kamer[i].b_add_mess_usl_more = 1
         # сообщение на выполнения условия занижения
         if kam.usl_send_less > 0 and kam.cnt_people < kam.usl_send_less and \
-           (kam.last_send_less_usl_dt == datetime(1900, 1, 1) or \
-            (datetime.now() - kam.last_send_less_usl_dt).total_seconds() >= min_inrerval_sec or \
-            (kam.last_send_less_norm_dt != datetime(1900, 1, 1) and kam.last_send_less_norm_dt > kam.last_send_less_usl_dt) or \
-            kam.cnt_people < kam.last_send_less_usl_cnt - kam.usl_change_min):
+           (
+             kam.last_send_less_usl_dt == datetime(1900, 1, 1) or
+             (datetime.now() - kam.last_send_less_usl_dt).total_seconds() >=
+             min_inrerval_sec or
+             (kam.last_send_less_norm_dt != datetime(1900, 1, 1) and
+              kam.last_send_less_norm_dt > kam.last_send_less_usl_dt) or
+             kam.cnt_people < kam.last_send_less_usl_cnt - kam.usl_change_min
+           ):
             spisok_kamer[i].b_add_mess_usl_less = 1
         # сообщение на выполнение условия нормализации при превышении
         if kam.usl_send_more > 0 and kam.cnt_people <= kam.usl_norm_more and \
            kam.last_send_more_usl_dt > datetime(1900, 1, 1) and \
-           (kam.last_send_more_norm_dt == datetime(1900, 1, 1) or \
-            kam.last_send_more_norm_dt < kam.last_send_more_usl_dt):
-            spisok_kamer[i].b_add_mess_usl_more_norm = 1  
+           (
+             kam.last_send_more_norm_dt == datetime(1900, 1, 1) or
+             kam.last_send_more_norm_dt < kam.last_send_more_usl_dt
+           ):
+            spisok_kamer[i].b_add_mess_usl_more_norm = 1
         # сообщение на выполнение условия нормализации при снижении
         if kam.usl_send_less > 0 and kam.cnt_people >= kam.usl_norm_less and \
            kam.last_send_less_usl_dt > datetime(1900, 1, 1) and \
-           (kam.last_send_less_norm_dt == datetime(1900, 1, 1) or \
-            kam.last_send_less_norm_dt < kam.last_send_less_usl_dt):
+           (
+             kam.last_send_less_norm_dt == datetime(1900, 1, 1) or
+             kam.last_send_less_norm_dt < kam.last_send_less_usl_dt
+           ):
             spisok_kamer[i].b_add_mess_usl_less_norm = 1
 
 
@@ -367,7 +399,7 @@ def result_write_base(spisok_kamer, conn):
             b_add_mess_usl_less = True
         if kam.b_add_mess_usl_less_norm == 1:
             b_add_mess_usl_less_norm = True
-    
+
         sql = f""" INSERT INTO calc_people_camera_cnt_people
                       (id_calc_people_camera, cnt_people, date_time,
                        b_add_mess_usl_more, b_add_mess_usl_more_norm,
